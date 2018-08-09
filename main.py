aa46ba1c1490b139fb0a1fd02e3700bd8079f9f5
Damir Jailaubayev <damir.jailaubayev@gmail.com>
2018-08-09 23:54:25 +0600

    add xml library, xml tree construct and loop for fill out info over xml tree

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from flask import Flask, render_templates, request, redirect, url_for
 from wtforms import Form, validators, StringField
 from wtforms.validators import DataRequired
 import itertools, datetime
+import xml.etree.ElementTree as ET
 
 app = Flask(__name__)
 app.config.from_object(__name__)
@@ -31,7 +32,7 @@ def data():
 		Shipment_id=request.form['Shipment_id']
 		Quantity_on_pallet = int(Quantity_on_pallet)
 		StartPalletNumber = int(StartPalletNumber)
-
+		return redirect(url_for('index'))
 
 	return render_templates("data.html", form=form)
 
@@ -65,6 +66,46 @@ def index():
 		Pallets = [str(item) for item in Pallets]
 		quantity = str(len(Pallets))
 
+		dt=str(datetime.datetime.now().strftime("%Y%m%d%H%M%S"))
+
+		asn = ET.Element("asn")
+		asn.set("xsi:noNamespaceSchemaLocation", "S:/SchemaRepository/XMLSchemas/External/asn.xsd")
+		asn.set("xmlns:xsi", "http://www.w3.org/2001/XMLSchema-instance")
+
+		sender_location = ET.SubElement(asn, "sender_location") 
+		sender_location.text=" "
+
+		receiver_location = ET.SubElement(asn, "receiver_location")
+		receiver_location.text=" "
+
+		shipment_id = ET.SubElement(asn, "shipment_id")
+		shipment_id.text = Shipment_id
+
+		shipment_date = ET.SubElement(asn, "shipment_date")
+		shipment_date.text = dt
+
+		vehicle_id = ET.SubElement(asn, "vehicle_id")
+		vehicle_id.text=" "
+
+		purchase_order = ET.SubElement(asn, "purchase_order")
+		purchase_order.text = Purchase_order
+
+		number_of_pallets = ET.SubElement(asn, "number_of_pallets")
+		number_of_pallets.text = quantity
+
+
+		count=0
+		for n in Pallets:
+			pallet = ET.SubElement(asn, "pallet")
+			sscc = ET.SubElement(pallet, "sscc")
+			sscc.text = n
+			number_of_cases = ET.SubElement(pallet, "number_of_cases")
+			number_of_cases.text = str(len(cases_a[y]))
+			for i in cases_a[y]:
+				case = ET.SubElement(pallet, "case")
+				label = ET.SubElement(case, "label")
+				label.text = i
+			count+=1
 
 
 
