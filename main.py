fd9c7fbff54133fcd118090ddae085bbf6dce85c
Damir Jailaubayev <damir.jailaubayev@gmail.com>
2018-08-09 19:34:11 +0600

    Add Data form route and data function

--- main.py
+++ main.py
@@ -15,7 +15,25 @@ class DataForm(Form):
 	Shipment_id = StringField('ShipmentId', validators=[validators.required(), validators.Length(min=10, max=10)])
 
 
+@app.route("/", methods=["POST"])
+def data():
+	form = DataForm(request.form)
 
+	global Quantity_on_pallet
+	global StartPalletNumber
+	global Purchase_order
+	global ShipmentId
+
+	if request.method == 'POST':
+		Quantity_on_pallet=request.form['Quantity_on_pallet']
+		StartPalletNumber=request.form['PalletNumber']
+		Purchase_order=request.form['Purchase_order']
+		Shipment_id=request.form['Shipment_id']
+		Quantity_on_pallet = int(Quantity_on_pallet)
+		StartPalletNumber = int(StartPalletNumber)
+
+
+	return render_templates("data.html", form=form)
 
 if __name__ == "__main__"
 	app.run(debug=True)
