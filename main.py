b17d623899af6c8ec03f9e223015d9c60a2d70c8
Damir Jailaubayev <damir.jailaubayev@gmail.com>
2018-08-09 18:44:24 +0600

    Add form class for input form

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
 from flask import Flask
 from wtforms import Form, validators, StringField
 from wtforms.validators import DataRequired
+
+
+
+class DataForm(Form):
+	
+	Quantity_on_pallet = StringField('Quantity', validators=[validators.required(), validators.Length(min=1, max=4)])
+	PalletNumber = StringField('PalletNumber', validators=[validators.required(), validators.Length(min=19, max=19)])
+	Purchase_order = StringField('PurchaseOrder', validators=[validators.required(), validators.Length(min=10, max=10)])
+	Shipment_id = StringField('ShipmentId', validators=[validators.required(), validators.Length(min=10, max=10)])
